d3bd1af52d6b48a980b6b29766bfd3767647ab68
Christopher Covington <cov@mykolab.com>
2025-04-26 15:44:41 -0400

    workaround

--- wellplated/management/commands/schema.py
+++ wellplated/management/commands/schema.py
@@ -4,6 +4,7 @@ from subprocess import check_output
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from sqlfluff import fix
+from sqlfluff.core.config import FluffConfig
 
 
 class Command(BaseCommand):
@@ -14,7 +15,8 @@ class Command(BaseCommand):
         print(f'Generating {output}')
         raw = check_output(
             ('sqlite3', settings.DATABASES['default']['NAME'], '.schema wellplated_*')
-        ).decode()
+        ).decode().replace('unsigned', '/*unsigned*/')
         # sqlparse.format doesn't do a good enough job
-        formatted = fix(raw, 'sqlite')
+        # Workaround https://github.com/sqlfluff/sqlfluff/issues/6844
+        formatted = fix(raw, 'sqlite', config=FluffConfig({'core': {'dialect': 'sqlite', 'max_line_length': 100}})).replace('/*unsigned*/', 'unsigned')
         output.write_text(formatted)
